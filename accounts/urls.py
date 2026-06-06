e7d0783c22a02b77edacf0cdcb190db64003ac9a
Bahar <fatemehkhosravi.002@gmail.com>
2026-06-06 18:47:01 +0330

    add endpoints for custom user create and get

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
+from .views import CustomUserDetailView, CustomUserCreateView
+
 urlpatterns=[
     path("token/", jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', CustomUserCreateView.as_vieW(), name="custom_user_create_view"),
+    path('me/', CustomUserDetailView.as_view(), name="custom_user_detail_view"),
 ]
