379919ec92fede6a26ab381eb66d4c70e445dc72
Bahar <fatemehkhosravi.002@gmail.com>
2026-06-10 19:08:11 +0330

    change db of proj to pastgres

--- core/settings.py
+++ core/settings.py
@@ -100,8 +100,12 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_POSR")
     }
 }
 
