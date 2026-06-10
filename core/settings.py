256a662244e87a06ee6ff3c52e65f3758861fa4e
Bahar <fatemehkhosravi.002@gmail.com>
2026-06-10 19:37:52 +0330

    edit postgres driver

--- core/settings.py
+++ core/settings.py
@@ -100,12 +100,12 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgres',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config("DATABASE_NAME"),
         'USER': config("DATABASE_USER"),
         'PASSWORD': config("DATABASE_PASSWORD"),
         'HOST': config("DATABASE_HOST"),
-        'PORT': config("DATABASE_POSR")
+        'PORT': config("DATABASE_PORT")
     }
 }
 
