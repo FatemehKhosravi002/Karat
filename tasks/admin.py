78ce24b7a6461659c99ae3607b301772ec117e70
Bahar <fatemehkhosravi.002@gmail.com>
2026-06-13 17:02:08 +0330

    add Tag model to admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 
-from .models import TaskModel
+from .models import TaskModel, TagModel
 
 @admin.register(TaskModel)
 class TaskAdmin(admin.ModelAdmin):
-    list_display=["title","user_name", "is_deleted"]
-    search_fields=("title",)
-    list_filter=("user",)
+    list_display = ["title","user_username", "is_deleted"]
+    search_fields = ("title",)
+    list_filter = ("user",)
 
-    def user_name(self, obj):
+    def user_username(self, obj):
         return obj.user.username
     
-    user_name.admin_order_field = "user__username"
+    user_username.admin_order_field = "user__username"
+
+
+@admin.register(TagModel)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name", "user_username"]
+
+    def user_username(self, obj):
+        return obj.user.username
+    
